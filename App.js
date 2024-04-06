d432c10bc99c4ce09fbe89c239c53863732474b8
myusername <127469454+Shubham-garg1234@users.noreply.github.com>
2024-04-06 12:29:54 +0530

    Corected error of not building production build 3

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const Data =()=>{
     return (<>
@@ -7,5 +7,5 @@ const Data =()=>{
     </>)
 }
 
-root = ReactDOM.createRoot(document.getElementById("root"))
+const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Data />)
