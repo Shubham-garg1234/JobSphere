14fbe7fb214de2132a3dc46d84c87b32ac52d42f
myusername <127469454+Shubham-garg1234@users.noreply.github.com>
2024-04-06 11:53:46 +0530

    first-commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+const Data =()=>{
+    return (<>
+    <h1>Hello</h1>
+    </>)
+}
+
+root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<Data />)
